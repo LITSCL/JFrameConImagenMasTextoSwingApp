048c0c5775249bc43c38b69bd72ede8d23f330a2
Daniel Álvarez <danielalvarez.litscl.melchiot@protonmail.com>
2023-12-06 11:17:53 -0300

    Ok

---
+++ src/cl/inacap/jframeconimagenmastextoswingapp/StartFrame.java
@@ -0,0 +1,45 @@
+package cl.inacap.jframeconimagenmastextoswingapp;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class StartFrame extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					StartFrame frame = new StartFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public StartFrame() {
+		setTitle("JFrameConImagenMasTextoSwingApp");
+		setIconImage(Toolkit.getDefaultToolkit().getImage("Imagenes/StartFrame.png"));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
